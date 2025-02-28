04407f0a03f8fad5c047baa6c2ac6d308e78cf0e
Guilherme bento da Silva <guilhermebentodasilva55@gmail.com>
2025-02-28 20:27:23 -0300

    Mod

--- App.ts
+++ App.ts
@@ -2,9 +2,5 @@ import Block from "./Domain/Blocos/Bloco";
 import { InitDataBase } from "./Infra/Context/AppDbContext.js";
 
 
-<<<<<<< HEAD
 InitDataBase()
 // Teste
-=======
-InitDataBase()
->>>>>>> e237cc5cb0395f00342ed76fee8a329e69c545d9
